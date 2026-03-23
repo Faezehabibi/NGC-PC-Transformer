f496c31e89e4785cb46775752256202f8b843bc8
Nardos Tatek <narditatek@gmail.com>
2026-03-23 14:02:06 +0100

    change model prediction from the projection output to inference prediction

--- tuning.py
+++ tuning.py
@@ -253,10 +253,10 @@ def run_phase2_trial(trial, best_params):
         targets_flat = jax.nn.one_hot(targets.flatten(), cfg.vocab_size)
 
         try:
-            yMu_inf, _, EFE, *_ = model.process(obs=inputs, lab=targets_flat, adapt_synapses=True)
+            yMu_inf, y_mu, EFE, *_ = model.process(obs=inputs, lab=targets_flat, adapt_synapses=True)
             EFE = abs(float(EFE))
             
-            y_pred = yMu_inf.reshape(-1, cfg.vocab_size)
+            y_pred = y_mu.reshape(-1, cfg.vocab_size)
             batch_nll = measure_CatNLL(y_pred, targets_flat) * targets_flat.shape[0]
             batch_train_ce = batch_nll / targets_flat.shape[0]
             
